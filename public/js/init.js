420b88f2861bbfb9d41724e4931ae3d4bf1e8d69
varbha <varunb95@gmail.com>
2017-02-05 22:18:49 +0530

    t&c modal, styling

--- public/js/init.js
+++ public/js/init.js
@@ -4,6 +4,28 @@
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('.modal').modal();
+    $('.terms').click(function(){
+	  $('button.cont').removeClass('disabled');	
+	  $('button.cont').addClass('enabled');	
+      $('span.me').removeAttr("hidden");
+	  $('i.dechanged').removeClass('red-text');
+      $('p.changed').css("color","green");
+	  $('i.dechanged').addClass('green-text');
+	  $('i.dechanged').html('done');
+    });
+	$('.noterms').click(function(){
+		$('button.cont').removeClass('enabled');
+		$('span.me').removeAttr("hidden");
+		$('p.changed').css("color","red");
+		$('i.dechanged').removeClass('green-text');
+		$('i.dechanged').addClass('red-text');
+		$('i.dechanged').html('error');
+		$('button.cont').addClass('disabled');	
+		
+		
+		
+		
+	});
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
